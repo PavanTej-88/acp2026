d878ad3ebdaadadac8924afee4fb7b5ae5db92ef
PavanTej <is.pavantejgamil.com>
2026-03-20 11:33:05 +0530

    act5.c: modified code for two different lengths of strings, tried to do swapping without using string function strcpy(), no errors, but I am accessing memory from out the bounds

--- level1/act5.c
+++ level1/act5.c
@@ -9,12 +9,14 @@ void read_string(char *str, int x);
 void print_string(char *str);
 void swap_strings(char *str1, char *str2);
 int main(){
-    int n;
-    printf("Enter size of string: ");
-    scanf("%d",&n);
-    char str1[n],str2[n];
-    read_string(str1,n);
-    read_string(str2,n);
+    int n1,n2;
+    printf("Enter size of string1: ");
+    scanf("%d",&n1);
+    printf("Enter size of string2: ");
+    scanf("%d",&n2);
+    char str1[n1],str2[n2];
+    read_string(str1,n1);
+    read_string(str2,n2);
     printf("Strings before swapping:\n");
     print_string(str1);
     print_string(str2);
@@ -25,23 +27,44 @@ int main(){
     return 0;
 }
 void read_string(char *s,int x){
-    printf("Enter string1{max_size=%d}: \n",x);
-    fgets(s,sizeof(s),stdin);
+    printf("Enter string{max_size=%d}: ",x);
+    scanf("%s",s);
 }
 void print_string(char *s){
-    printf("%s",s);
+    printf("%s\n",s);
 }
 void swap_strings(char *x,char *y){
     int i=0;
-    char temp;
-    while((x[i]!='\0')&&(y[i]!='\0')){
-        temp=x[i];
-        x[i]=y[i];
-        y[i]=temp;
-        i++;
+    if(strlen(x)>strlen(y)){
+        int k=strlen(x);
+        char temp[k];
+        for(i=0;i<=k;i++){
+            temp[i]=x[i];
+            x[i]=y[i];
+            y[i]=temp[i];
+        }
     }
-}
 
+        
+    else if(strlen(y)>strlen(x)){
+        int k=strlen(y);
+        char temp[k];
+        for(i=0;i<=k;i++){
+            temp[i]=y[i];
+            y[i]=x[i];
+            x[i]=temp[i];
+        }
+    }
+    else{
+        int k=strlen(y);
+        char temp[k];
+        for(i=0;i<=k;i++){
+            temp[i]=y[i];
+            y[i]=x[i];
+            x[i]=temp[i];
+        }
+}
+}
 
 
 
