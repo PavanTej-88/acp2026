1fc34892741b9ddaa209ba153d17b5cc979a6098
PavanTej <is.pavantejgamil.com>
2026-03-24 14:43:23 +0530

    asg7.c-first compilation-errors-used double quotes instead of single quotes for a space character, it became a pointer

---
+++ level1/asg7.c
@@ -0,0 +1,72 @@
+/*Write a C program to count how many words in a given sentence. 
+Use a user-defined function to perform the counting. The sentence may 
+contain multiple words separated by spaces. (strings, while/do while)
+Function Prototypes:
+void input(char *str);
+int count_words(char *str);
+void display(char *str, int count);*/
+#include<stdio.h>
+#include<string.h>
+void input(char *str);
+int count_words(char *str);
+void display(char *str, int count);
+int main(){
+    char s[100];
+    input(s);
+    int w=count_words(s);
+    display(s,w);
+    return 0;
+}
+void input(char *x){
+    printf("Enter sentence: ");
+    fgets(x,sizeof(x),stdin);
+}
+int count_words(char *x){
+    int i,w=0;
+    while((x[i]!='\0')&&(x[i]!='\n')){
+        if(x[i]==" "){
+            w++;
+        }
+        i++;
+    }
+    return w;
+}
+void display(char *x,int w){
+    printf("Number of spaces in the sentence '%s' is %d",x,w);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
