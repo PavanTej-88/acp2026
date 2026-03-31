ec2c7c8fd9ac48c5b4459ea1c9ce77374905e22e
PavanTej <is.pavantejgamil.com>
2026-03-31 07:23:13 +0530

    completed demo9.c

--- level1/demo9.c
+++ level1/demo9.c
@@ -9,6 +9,7 @@ void writeToTextFile(Student students[], int n, const char *filename);
 int readFromTextFile(Student students[], const char *filename);
 void printStudents(Student students[], int n);*/
 #include<stdio.h>
+#include<stdlib.h>
 typedef struct{
     char n[50];
     int sid;
@@ -23,6 +24,10 @@ int main(){
     //const char f[50]="student.txt";
     printf("Enter number of students: ");
     scanf("%d",&n);
+    if(n<=0){
+        printf("Invalid number of students\n");
+        return -1;
+    }
     Student s[n];
     inputStudents(s,n);
     writeToTextFile(s,n,"students.txt");
@@ -33,6 +38,7 @@ int main(){
 }
 void inputStudents(Student *s,int n){
     for(int i=0;i<n;i++){
+        printf("Enter student %d details\n",i+1);
         printf("Enter student name: ");
         scanf("%s",s[i].n);
         printf("Enter student id: ");
@@ -44,6 +50,10 @@ void inputStudents(Student *s,int n){
 void writeToTextFile(Student *s,int n,const char *f){
     FILE *ptr;
     ptr=fopen(f,"w");
+    if(ptr==NULL){
+        printf("Error opening file\n");
+        exit(-1);
+    }
     for(int i=0;i<n;i++){
         fprintf(ptr,"%s %d %d",s[i].n,s[i].sid,s[i].m);
     }
@@ -53,13 +63,23 @@ int readFromTextFile(Student *s1,const char *f){
     FILE *ptr;
     int i=0;
     ptr=fopen(f,"r");
+    if(ptr==NULL){
+        printf("Error opening file\n");
+        exit(-1);
+    }
     while((fscanf(ptr,"%s %d %d",s1[i].n,&s1[i].sid,&s1[i].m))==3){
         i++;
     }
     fclose(ptr);
+    printf("Read %d students\n",i);
+    for(int j=0;j<20;j++){
+        printf("=");
+    }
+    printf("\n");
     return i;
 }
 void printStudents(Student *s1,int x){
+    printf("%d students details:\n",x);
     for(int i=0;i<x;i++){
         printf("Student %d:\n",i+1);
         printf("Name\tId\tMarks\n");
